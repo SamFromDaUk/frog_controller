ac35a1c5c5f2f9440c2ab7b34db9bffa9d65043a
Sam Warren <samwarren909@gmail.com>
2013-10-13 00:49:15 +0100

    Upgrade file now runs in the upgrade.html file

--- app/public/content-scripts/upgrade.js
+++ app/public/content-scripts/upgrade.js
@@ -1,5 +1,30 @@
+var config,
+    app;
+
+app = {
+    initUpgrade: function() {
+        $('.upgrade').html('<iframe src="'+ config.upgrade +'"></iframe>');
+
+        $('.upgrade iframe').load(function() {
+
+            if (config.flags.initial) {
+                app.initInitial();
+            }
+        });
+    },
+
+    initInitial: function() {
+
+        $('.upgrade').css('right', '51%');
+        $('.initial').show().html('<iframe src="'+ config.initial +'"></iframe>');
+    }
+};
+
 $(function() {
     chrome.runtime.onMessage.addListener(function(configuration) {
+        config = configuration;
+        console.log('Config', config);
 
+        app.initUpgrade();
     });
 });
