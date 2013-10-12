8c2b3d4dfab1a39928f0d4e2e9cb9ec68a4d33e6
Sam Warren <samwarren909@gmail.com>
2013-10-13 00:58:33 +0100

    Initial script is now running

--- app/core/models/setup_model.js
+++ app/core/models/setup_model.js
@@ -2,7 +2,7 @@ App.Models.Url = can.Model.extend({
     getUrl: function(type) {
         var map = {
             'upgrade': 'lib/_upgrade.php',
-            'setup': 'setup/initial.php'
+            'initial': 'setup/initial.php'
         };
 
         if (map[type]) {
--- app/public/content-scripts/upgrade.js
+++ app/public/content-scripts/upgrade.js
@@ -3,6 +3,13 @@ var config,
 
 app = {
     initUpgrade: function() {
+        window.title = 'DB Upgrading';
+
+        if (config.flags.initial) {
+            $('.upgrade').css('right', '51%');
+            $('.initial').show();
+        }
+
         $('.upgrade').html('<iframe src="'+ config.upgrade +'"></iframe>');
 
         $('.upgrade iframe').load(function() {
@@ -14,9 +21,12 @@ app = {
     },
 
     initInitial: function() {
+        window.title = 'Setting up users';
+        $('initial').html('<iframe src="'+ config.initial +'"></iframe>');
 
-        $('.upgrade').css('right', '51%');
-        $('.initial').show().html('<iframe src="'+ config.initial +'"></iframe>');
+        $('.initial iframe').load(function() {
+            window.title = 'Upgrade complete';
+        });
     }
 };
 
